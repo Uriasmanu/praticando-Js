bed0ed16d429c68ac326283ddbd370171db4afae
MANOELA Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-01-16 22:33:27 -0300

    atualização e finalização

---
+++ main.js
@@ -0,0 +1,11 @@
+var click = document.getElementById("botton");
+
+click.addEventListener ("click",(event) => {
+   
+    alert("Eba fui clicado");
+})
+/* Codigo sugerido
+function  clicou ( )  {
+    window.alert ( 'Eba você clicou' )
+}
+*/
